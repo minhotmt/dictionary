3ca3d6732b2a946e0fc65cef65d287a60aeafd1f
minhotmt <minhotmt@gmail.com>
2019-01-10 14:05:37 +0700

    Update Translator API

--- app/src/main/java/com/example/minko/dictionaryclone/Fragment/TranslatorFragment.java
+++ app/src/main/java/com/example/minko/dictionaryclone/Fragment/TranslatorFragment.java
@@ -1,21 +1,30 @@
 package com.example.minko.dictionaryclone.Fragment;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.example.minko.dictionaryclone.R;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+
 import java.util.Locale;
 
 /**
@@ -26,13 +35,14 @@ import java.util.Locale;
  * Use the {@link TranslatorFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TranslatorFragment extends Fragment implements TextToSpeech.OnInitListener{
+public class TranslatorFragment extends Fragment implements TextToSpeech.OnInitListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private TextToSpeech tts;
-    private EditText txtText;
+    private EditText edtText;
+    private TextView txtWord;
     private ImageButton iconListen;
 
     // TODO: Rename and change types of parameters
@@ -40,6 +50,7 @@ public class TranslatorFragment extends Fragment implements TextToSpeech.OnInitL
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+//    private MyAsyncTask myAsyncTask;
 
     public TranslatorFragment() {
         // Required empty public constructor
@@ -78,7 +89,7 @@ public class TranslatorFragment extends Fragment implements TextToSpeech.OnInitL
 
         View view = inflater.inflate(R.layout.fragment_translator, container, false);
         // Inflate the layout for this fragment
-
+        txtWord = view.findViewById(R.id.txtWord);
         ImageButton imageButton = view.findViewById(R.id.imgChange);
         final ImageButton iconListen = view.findViewById(R.id.icon_listen);
         final ImageButton iconDelete = view.findViewById(R.id.imgDelete);
@@ -89,7 +100,7 @@ public class TranslatorFragment extends Fragment implements TextToSpeech.OnInitL
         iconDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txtText.setText("");
+                edtText.setText("");
             }
         });
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -107,49 +118,48 @@ public class TranslatorFragment extends Fragment implements TextToSpeech.OnInitL
         });
 
         tts = new TextToSpeech(getContext(), this);
-        txtText = view.findViewById(R.id.edtText);
-        txtText.addTextChangedListener(new TextWatcher() {
+        edtText = view.findViewById(R.id.edtText);
+//        myAsyncTask = new MyAsyncTask(getActivity());
+        edtText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(!txtText.getText().toString().equals("")){
+                if (!edtText.getText().toString().equals("")) {
                     iconListen.setVisibility(View.VISIBLE);
                 } else {
                     iconListen.setVisibility(View.INVISIBLE);
+                    txtWord.setText("");
                 }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-
             }
 
         });
 
-//        Translate translate = TranslateOptions.getDefaultInstance().getService();
-        // The text to translate
-        String text = "Hello, world!";
-        // Translates some text into Russian
-//        Translation translation =
-//                translate.translate(
-//                        text,
-//                        Translate.TranslateOption.sourceLanguage("en"),
-//                        Translate.TranslateOption.targetLanguage("ru"));
-//        Toast.makeText(getContext(), translation.toString(), Toast.LENGTH_SHORT).show();
-
-
+        edtText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    new MyAsyncTask(getActivity()).execute(edtText.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
         return view;
     }
 
 
-
-    public void change(ImageView img, TextView txt){
+    public void change(ImageView img, TextView txt) {
         String a = txt.getText().toString();
-        if (a.equals("English")){
+        if (a.equals("English")) {
             img.setImageResource(R.drawable.icon_russian);
             txt.setText("Russian");
         } else {
@@ -199,6 +209,7 @@ public class TranslatorFragment extends Fragment implements TextToSpeech.OnInitL
             Log.e("TTS", "Initilization Failed!");
         }
     }
+
     @Override
     public void onDestroy() {
         // Don't forget to shutdown tts!
@@ -208,10 +219,27 @@ public class TranslatorFragment extends Fragment implements TextToSpeech.OnInitL
         }
         super.onDestroy();
     }
+
     private void speakOut() {
-        String text = txtText.getText().toString();
+        String text = edtText.getText().toString();
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
+
+    public String Translator(String text) {
+        TranslateOptions options = TranslateOptions.newBuilder()
+                .setApiKey("AIzaSyCesRSQ_xOlB489lpIZvOxSBFRPv6a_lNk")
+                .build();
+        com.google.cloud.translate.Translate translate = options.getService();
+        try {
+            Translation translation = translate.translate(text, // dogings[0] = "dog"
+                    com.google.cloud.translate.Translate.TranslateOption.sourceLanguage("en"), // en
+                    com.google.cloud.translate.Translate.TranslateOption.targetLanguage("ru")); // vi
+            return translation.getTranslatedText();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -225,5 +253,57 @@ public class TranslatorFragment extends Fragment implements TextToSpeech.OnInitL
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public class MyAsyncTask extends AsyncTask<String, String, Void> {
+
+        Activity contextParent;
+//        EditText edtWord;
+
+        public MyAsyncTask(Activity contextParent) {
+            this.contextParent = contextParent;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //Hàm này sẽ chạy đầu tiên khi AsyncTask này được gọi
+            //Ở đây mình sẽ thông báo quá trình load bắt đâu "Start"
+//            Toast.makeText(contextParent, "Start", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            //Hàm được được hiện tiếp sau hàm onPreExecute()
+            //Hàm này thực hiện các tác vụ chạy ngầm
+            //Tuyệt đối k vẽ giao diện trong hàm này
+            String a = Translator(params[0]);
+            publishProgress(a);
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            //Hàm thực hiện update giao diện khi có dữ liệu từ hàm doInBackground gửi xuống
+            super.onProgressUpdate(values);
+            //Thông qua contextCha để lấy được control trong MainActivity
+//            ProgressBar progressBar = (ProgressBar) contextParent.findViewById(R.id.prbDemo);
+            //vì publishProgress chỉ truyền 1 đối số
+            //nên mảng values chỉ có 1 phần tử
+            String mean = values[0];
+            //tăng giá trị của Progressbar lên
+//            progressBar.setProgress(number);
+            //đồng thời hiện thị giá trị là % lên TextView
+            TextView textView = (TextView) contextParent.findViewById(R.id.txtWord);
+            textView.setText(mean);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            //Hàm này được thực hiện khi tiến trình kết thúc
+            //Ở đây mình thông báo là đã "Finshed" để người dùng biết
+            //Toast.makeText(contextParent, "Okie, Finished", Toast.LENGTH_SHORT).show();
+        }
     }
 }
